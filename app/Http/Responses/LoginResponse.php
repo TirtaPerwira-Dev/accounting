79eb65fea771796bb78fde479ee88dc098cb4344
Fachri Mardliana <fachrimardliana16@gmail.com>
2026-01-12 14:49:29 +0700

    fix: Remove type hint from toResponse method in LoginResponse for compatibility

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -3,11 +3,10 @@
 namespace App\Http\Responses;
 
 use Filament\Http\Responses\Auth\Contracts\LoginResponse as LoginResponseContract;
-use Illuminate\Http\RedirectResponse;
 
 class LoginResponse implements LoginResponseContract
 {
-    public function toResponse($request): RedirectResponse
+    public function toResponse($request)
     {
         $user = auth()->user();
 
