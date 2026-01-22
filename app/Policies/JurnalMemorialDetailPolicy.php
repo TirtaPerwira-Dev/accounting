8792be6c0368f0a7bdef4ad8c77565097a125245
alisnapro <workspace.auliamaruf@gmail.com>
2026-01-22 08:22:49 +0700

    feat: Add policy for Jurnal Pemakaian Bahan Detail with authorization methods

---
+++ app/Policies/JurnalMemorialDetailPolicy.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\JurnalMemorialDetail;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class JurnalMemorialDetailPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, JurnalMemorialDetail $jurnalMemorialDetail): bool
+    {
+        return $user->can('view_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, JurnalMemorialDetail $jurnalMemorialDetail): bool
+    {
+        return $user->can('update_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, JurnalMemorialDetail $jurnalMemorialDetail): bool
+    {
+        return $user->can('delete_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     */
+    public function forceDelete(User $user, JurnalMemorialDetail $jurnalMemorialDetail): bool
+    {
+        return $user->can('force_delete_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     */
+    public function restore(User $user, JurnalMemorialDetail $jurnalMemorialDetail): bool
+    {
+        return $user->can('restore_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     */
+    public function replicate(User $user, JurnalMemorialDetail $jurnalMemorialDetail): bool
+    {
+        return $user->can('replicate_jurnal::memorial');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_jurnal::memorial');
+    }
+}
